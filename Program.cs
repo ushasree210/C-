694ed4cf287e1dea0d209b378f6216cadd783126
ushasree210 <79778957+ushasree210@users.noreply.github.com>
2023-07-08 22:04:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayreversename
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENter the name");
+
+    var name=Console.ReadLine();
+            var namy = new char[name.Length];
+            for (int i = 0; i<name.Length; i++)
+                namy[name.Length-i-1] = name[i];
+            var reversed = namy;
+            Console.WriteLine(reversed); 
+        }
+    }
+}
